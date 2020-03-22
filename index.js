48bc37059a8a239356eb97ec0e9aed3f264cd716
edward <son@binhsonnguyen.com>
2020-03-22 23:44:15 +0700

    stop log secret values

--- index.js
+++ index.js
@@ -4,11 +4,6 @@ const axios = require('axios')
 const express = require('express')
 
 const listener = express().listen(0, () => {
-  axios.interceptors.request.use(request => {
-    console.log('Starting Request', request.url, request.data)
-    return request
-  })
-
   new IpInfoTwitter().start()
   console.log(`Example app listening on port ${listener.address().port}!`)
 })
